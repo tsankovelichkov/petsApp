e89338d89b193f2ec2f6b4a455314975b5938cf1
tsankovelichkov <80650325+tsankovelichkov@users.noreply.github.com>
2022-01-12 16:04:26 +0200

    creating Main and Footer component

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div id="container">
       <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <>
+            <footer id="site-footer">
+                <p>@PetMyPet</p>
+            </footer>
+            <style jsx>{``}</style>
+        </>
+    );
+}
+
+export default Footer;
